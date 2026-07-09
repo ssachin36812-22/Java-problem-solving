a1b7fc6c9923e9cadb0ebe73344b626b70737d11
Sachin M <ssachin36812@gmail.com>
2026-07-09 12:10:06 +0530

    This Program Coding For Day1

---
+++ Day12/BankingTest.java
@@ -0,0 +1,18 @@
+package Day12;
+
+import java.util.Arrays;
+
+public class BankingTest {
+
+        public static void main(String[] args){
+            String[]menu={"Veg😔","nonveg☹️","sweet😖","beverages😡"};
+
+            String[]menu1=new String[menu.length];
+            for(int i=0;i<menu.length;i++){
+                menu1[i]=menu[menu.length-1-i];
+            }
+            System.out.print(Arrays.toString(menu1));
+        }
+
+
+    }
